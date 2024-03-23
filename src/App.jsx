8e29f6275e85e8509bc0bc9185a0ad7fe9ea59c9
Chathura J Ekanayake <chathuraje01@gmail.com>
2024-03-23 16:13:16 +0530

    Day 1.7: Reusability of Components and Usage of Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import Hello from "./components/Hello";
 
 function App() {
+  const seatNumbers = [1, 2, 3, 4];
+
+  const favourites = {
+    food: "Pizza",
+    drink: "Coke",
+  };
+
   return (
     <div className="App">
-      <Hello />
+      <Hello
+        name="Chathura"
+        age="25"
+        seatNumbers={seatNumbers}
+        favourites={favourites}
+      />
     </div>
   );
 }
--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,18 +1,28 @@
-const name = "Chathura";
-
-function displayMessage() {
-  return "This is a function";
-}
-
-function Hello() {
+function Hello({ name, age, numbers, favourites }) {
   return (
     <div>
-      <h1>
-        {name}: This is a Component! {10 + 10}
-      </h1>
-      <h2>Function: {displayMessage()}</h2>
+      <h1>Hello {name}!</h1>
+      <h2>Age: {age}</h2>
+      <p>Seat Numbers: {numbers}</p>
+
+      <p>
+        Favourites: Food: {favourites.food} / Drink: {favourites.food}
+      </p>
     </div>
   );
 }
+
+// Can also use like this too
+// function Hello(props) {
+//   const { name, age } = props;
+
+//   return (
+//     <div>
+//       {/* Can also use like props.name and props.age */}
+//       <h1>Hello {name}!</h1>
+//       <h2>Age: {age}</h2>
+//     </div>
+//   );
+// }
 
 export default Hello;

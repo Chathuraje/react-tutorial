8051bad2e9f5c3bc583669f771ec2248a1bb7c38
Chathura J Ekanayake <chathuraje01@gmail.com>
2024-03-23 14:49:29 +0530

    Day 1.5: Create a React Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import Hello from './components/Hello';
+
 function App() {
-  return <div className="App">Hello World!</div>
+  return (
+    <div className="App">
+      <Hello />
+      <Hello></Hello>
+    </div>
+  );
 }
 
 export default App
---
+++ src/components/Hello.jsx
@@ -0,0 +1,6 @@
+function Hello() {
+    return <h1>This is a Component!</h1>;
+  }
+  
+  export default Hello
+  

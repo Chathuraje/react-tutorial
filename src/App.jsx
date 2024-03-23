4bdd47844f1cdb1c4b7a7f98751a0d7941d7d6be
Chathura J Ekanayake <chathuraje01@gmail.com>
2024-03-23 15:11:13 +0530

    Day 1.6: What is JSX (Javascript Extension) in React

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import Hello from './components/Hello';
+import Hello from "./components/Hello";
 
 function App() {
   return (
     <div className="App">
       <Hello />
-      <Hello></Hello>
     </div>
   );
 }
 
-export default App
+export default App;
--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,6 +1,18 @@
+const name = "Chathura";
+
+function displayMessage() {
+  return "This is a function";
+}
+
 function Hello() {
-    return <h1>This is a Component!</h1>;
-  }
-  
-  export default Hello
-  
+  return (
+    <div>
+      <h1>
+        {name}: This is a Component! {10 + 10}
+      </h1>
+      <h2>Function: {displayMessage()}</h2>
+    </div>
+  );
+}
+
+export default Hello;

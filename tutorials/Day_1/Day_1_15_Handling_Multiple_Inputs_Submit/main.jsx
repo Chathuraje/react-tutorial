26ed906c52d00aacd853a22576a27e295c6d69a2
Chathura J Ekanayake <chathuraje01@gmail.com>
2024-03-24 14:02:53 +0530

    fix imports issue

--- tutorials/Day_1/Day_1_15_Handling_Multiple_Inputs_Submit/main.jsx
+++ tutorials/Day_1/Day_1_15_Handling_Multiple_Inputs_Submit/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import App from "./App";
+import ReactDOM from "react-dom/client";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   //This is for development only - <React.StrictMode> </React.StrictMode>

ec10cf1ffe450e960a6c533f5375e899273789ef
Chathura J Ekanayake <chathuraje01@gmail.com>
2024-03-27 22:47:47 +0530

    Day 5.6: Synchronous Execution with useLayoutEffect

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,32 @@
-import { useDeferredValue, useState } from "react";
-import { SlowList } from "../components/SlowList";
+import { useLayoutEffect, useState } from "react";
+
+const userIds = [1, 2];
 
 export const Home = () => {
-  const [query, setQuery] = useState("");
-  const defeeredQuery = useDeferredValue(query);
+  const [userId, setUserId] = useState(userIds[0]);
+  const [isAdmin, setIsAdmin] = useState(true);
+
+  // This artifically slows down rendering
+  let now = performance.now();
+  while (performance.now() - now < 200) {
+    // Do nothing for bit
+  }
+
+  useLayoutEffect(() => {
+    setIsAdmin(userId === userIds[0]);
+  }, [userId]);
+
+  const handleChange = () => {
+    const otherId = userIds.find((id) => id !== userId);
+    setUserId(otherId);
+  };
 
   return (
     <div>
-      <h1>useDeferredValue</h1>
-      <input
-        type="text"
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-        placeholder="Search"
-      />
+      <h1>UserID: {userId}</h1>
+      <h3>Admin: {isAdmin ? "true" : "false"}</h3>
 
-      <SlowList text={defeeredQuery} />
+      <button onClick={handleChange}>Change User</button>
     </div>
   );
 };

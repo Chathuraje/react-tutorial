a173cb56012e4723aaf960da87c3662c63d7d18e
Chathura J Ekanayake <chathuraje01@gmail.com>
2024-03-27 23:52:28 +0530

    Day 5.8: Migration to TypeScript

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,5 @@
-import { Button, TextField, AppBar, Toolbar, Typography } from '@mui/material';
-
 function Home() {
-  return (
-    <div>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            My App
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-      <TextField label="Enter your name" />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default Home;
---
+++ tutorials/Day_5/Day_6_6_Use_Of_MUI/pages/Home.jsx
@@ -0,0 +1,19 @@
+import { Button, TextField, AppBar, Toolbar, Typography } from "@mui/material";
+
+function Home() {
+  return (
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            My App
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+      <TextField label="Enter your name" />
+    </div>
+  );
+}
+
+export default Home;
